6988b80cf35bf691ca3f8eca5dcc589f234dcd4a
Muhammadd08 <sobhymuhammad@gmail.com>
2023-07-13 18:33:08 -0700

    0x00. C - Hello, World

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ *main - Print a String
+ *
+ *Description: using puts
+ *
+ *Return: 0 (success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
