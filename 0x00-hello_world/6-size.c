4497e35acd12e5bfed46ffaac731e2a44d7b94d5
Muhammadd08 <sobhymuhammad@gmail.com>
2023-07-13 19:18:42 -0700

    0x00. C - Hello, World

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,7 +2,7 @@
 
 /**
  * main - Prints sized of characters
- * 
+ *
  * Description:  a C program that prints the size of various types
  *
  * Return: Always 0 (success)
@@ -10,7 +10,7 @@
 int main(void)
 {
 	printf("Size of a char: %zu byte(s)\n", sizeof(char));
-	printf("Size of a int: %zu byte(s)\n", sizeof(int));
+	printf("Size of an int: %zu byte(s)\n", sizeof(int));
 	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
 	printf("Size of a float: %zu byte(s)\n", sizeof(float));
