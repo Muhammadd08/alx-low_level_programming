bc30bda037dc7f290e3d24a8f29c39a162dc435b
Muhammadd08 <sobhymuhammad@gmail.com>
2023-07-25 15:36:45 -0700

    g

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,4 +1,9 @@
 #include "main.h"
+/**
+ * puts_half - prints half of a string
+ * followed by a new line
+ * @str: string to be printed
+ */
 
 void puts_half(char *str)
 {
@@ -12,7 +17,7 @@ void puts_half(char *str)
 	{
 		i++;
 	}
-	len = i;
+	len = i - 1;
 	if (len % 2 == 0)
 	{
 		len2 = len / 2;
