709b49fa29a573f377d29a1c9e995b5b08f372ab
Muhammadd08 <sobhymuhammad@gmail.com>
2023-07-26 17:53:21 -0700

    test

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -19,9 +19,10 @@ char *_strncat(char *dest, char *src, int n)
 	{
 		i++;
 	}
-	for (j = 0; j < n; j++, i++)
+	for (j = 0; j < n && src[j] != '\0'; j++, i++)
 	{
 		dest[i] = src[j];
 	}
+	dest[i] = '\0';
 	return (dest);
 }
