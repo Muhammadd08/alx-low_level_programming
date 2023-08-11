cf3248784a2087481ce4d0531ffeeacc5d69217a
Muhammadd08 <sobhymuhammad@gmail.com>
2023-08-10 18:06:37 -0700

    1. string_nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,7 +1,5 @@
 #include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
-
 /**
  * string_nconcat - concatenates two strings.
  * @s1: first string.
@@ -12,25 +10,20 @@
  * which contains s1, followed by the first n bytes of s2,
  * and null terminated.
  */
-
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	unsigned int count1 = 0, count2 = 0, i = 0, j = 0;
 	char *ptr;
 
-	while (s1[count1] != '\0')
-	{
-		count1++;
-	}
-	while (s2[count2] != '\0')
-	{
-		count2++;
-	}
+	if (s1 != NULL)
+		for (; s1[count1]; count1++)
+			;
+	if (s2 != NULL)
+		for (; s2[count2]; count2++)
+			;
 	ptr = malloc(sizeof(char) * (count1 + 1));
 	if (ptr == NULL)
-	{
 		return (NULL);
-	}
 	while (s1[i] != '\0')
 	{
 		ptr[i] = s1[i];
@@ -39,6 +32,8 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (n < count2)
 	{
 		ptr = realloc(ptr, sizeof(char) * (count1 + n + 1));
+		if (ptr == NULL)
+			return (NULL);
 		for (j = 0; j <= n; j++, i++)
 		{
 			ptr[i] = s2[j];
@@ -47,6 +42,8 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	else
 	{
 		ptr = realloc(ptr, sizeof(char) * (count1 + count2 + 1));
+		if (ptr == NULL)
+			return (NULL);
 		for (j = 0; s2[j] != '\0'; j++, i++)
 		{
 			ptr[i] = s2[j];
