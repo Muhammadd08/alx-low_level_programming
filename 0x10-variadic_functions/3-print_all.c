cab6a8f27a4cc8af23a1715c8a7a0bbb96b5b979
Muhammadd08 <sobhymuhammad@gmail.com>
2023-08-21 11:01:39 -0700

    3. To be is a to be the value of a variable

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,94 @@
+#include "variadic_functions.h"
+#include <stdlib.h>
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * print_char - print char.
+ * @sep: separator.
+ * @pc: argument pointer.
+ *
+ * Return: Always 0.
+ */
+void print_char(char *sep, va_list pc)
+{
+	printf("%s%c", sep, va_arg(pc, int));
+}
+
+/**
+ * print_integer - print integer.
+ * @sep: separator.
+ * @pi: argument pointer.
+ *
+ * Return: Always 0.
+ */
+void print_integer(char *sep, va_list pi)
+{
+	printf("%s%d", sep, va_arg(pi, int));
+}
+
+/**
+ * print_string - print string.
+ * @sep: separator.
+ * @ps: argument pointer.
+ *
+ * Return: Always 0.
+ */
+void print_string(char *sep, va_list ps)
+{
+	char *stg = va_arg(ps, char *);
+
+	if (stg == NULL)
+		stg = "(nil)";
+	printf("%s%s", sep, stg);
+}
+
+/**
+ * print_float - print float.
+ * @sep: separator.
+ * @pf: argument pointer.
+ *
+ * Return: Always 0.
+ */
+void print_float(char *sep, va_list pf)
+{
+	printf("%s%f", sep, va_arg(pf, double));
+}
+
+/**
+ * print_all - Entry Point
+ * @format: a list of types of arguments passed to the function.
+ *
+ * Return: Always 0.
+ */
+void print_all(const char *const format, ...)
+{
+	va_list list;
+	char *separator = "";
+	int j, i = 0;
+	type types[] = {
+		{"c", print_char},
+		{"i", print_integer},
+		{"d", print_integer},
+		{"s", print_string},
+		{"f", print_float}
+	};
+	va_start(list, format);
+
+	while (format && format[i])
+	{
+		j = 0;
+		while (j < 5)
+		{
+			if (types[j].z[0] == format[i])
+			{
+				types[j].pf(separator, list);
+				separator = ", ";
+			}
+			j++;
+		}
+		i++;
+	}
+	va_end(list);
+	printf("\n");
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,6 +1,13 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
 
+#include <stdarg.h>
+typedef struct x
+{
+        char *z;
+        void (*pf)(char *, va_list);
+} type;
+void print_all(const char * const format, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 int _putchar(char c);
