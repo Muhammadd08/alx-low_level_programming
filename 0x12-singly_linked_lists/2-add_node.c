9105a7c7d99b893b1779a63a75310fa3c2f719e4
Muhammadd08 <sobhynuhammad8@gmail.com.com>
2023-09-22 00:11:04 -0700

    J

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -17,6 +17,11 @@ list_t *add_node(list_t **head, const char *str)
 	if (new == NULL)
 		return (NULL);
 	new->str = strdup(str);
+	if (new->str == NULL)
+	{
+		free (new);
+		return (NULL)
+	}
 	new->next = *head;
 	*head = new;
 	return (new);
