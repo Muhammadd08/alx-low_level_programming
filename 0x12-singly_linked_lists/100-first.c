1fa0e4df78ca0629211fe2ff9be1c094baba81d7
Muhammadd08 <sobhynuhammad8@gmail.com.com>
2023-09-22 11:14:17 -0700

    G

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * afterMain - print string befor,
+ * the main function is executed.
+ * Return: Always 0.
+ */
+
+void afterMain(void)
+{
+	printf("You're beat! and yet, you must allow,");
+	printf("\nI bore my house upon my back!\n");
+}
