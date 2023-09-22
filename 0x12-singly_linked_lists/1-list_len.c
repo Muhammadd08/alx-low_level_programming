e9b3645b29affb0473f975650903583674b5377b
Muhammadd08 <sobhynuhammad8@gmail.com.com>
2023-09-21 18:41:31 -0700

    K

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,13 +1,13 @@
 #include "lists.h"
 
 /**
- * print_list - prints all the elements of a list_t list.
+ * list_len - returns the number of elements in a linked list_t list.
  * @h: singly linked list.
  *
  * Return: the number of nodes.
  */
 
-size_t print_list(const list_t *h)
+size_t list_len(const list_t *h)
 {
 	list *ptr;
 	size_t Counter = 0;
