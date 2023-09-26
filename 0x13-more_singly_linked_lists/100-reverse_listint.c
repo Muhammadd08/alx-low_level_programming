11e37e967897f7688be3a9c90569d45b53318a55
Muhammadd08 <sobhynuhammad8@gmail.com.com>
2023-09-26 03:01:30 -0700

    D

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -14,6 +14,13 @@ listint_t *reverse_listint(listint_t **head)
 		return (NULL);
 	if ((*head)->next == NULL)
 		return ((*head));
+	if ((*head)->next->next == NULL)
+	{
+		(*head) = (*head)->next;
+		(*head)->next = ptr;
+		tmp->next = NULL;
+		return ((*head));
+	}
 	(*head) = (*head)->next->next;
 	ptr = ptr->next;
 	tmp->next = NULL;
