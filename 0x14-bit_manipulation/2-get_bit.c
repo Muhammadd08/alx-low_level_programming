21a3d1add5fcb8141e4161ebb7db1b74a6b48edb
Muhammadd08 <sobhynuhammad8@gmail.com.com>
2023-10-01 06:51:41 -0700

    D

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,20 +1,5 @@
 #include "main.h"
 /**
- * _shifts - calculate number of digits.
- * @n: number we want to calculate it's digit.
- * Return: number of digits.
- */
-unsigned long int _shifts(unsigned long int n)
-{
-	unsigned long int i;
-
-	for (i = 0; n >= 1; i++)
-	{
-		n = n >> 1;
-	}
-	return (i - 1);
-}
-/**
  * get_bit - returns the value of a bit at a given index.
  * @n: number we want to calculate it's digit.
  * @index: the index, starting from 0 of the bit you want to get.
@@ -23,10 +8,9 @@ unsigned long int _shifts(unsigned long int n)
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	unsigned long int i, s;
+	unsigned int i;
 
-	s = _shifts(n);
-	if (index > s)
+	if (index > 31)
 		return (-1);
 	for (i = 0; i != index; i++)
 		n = n >> 1;
