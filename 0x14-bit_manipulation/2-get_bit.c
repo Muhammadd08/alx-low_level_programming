b738df7acd7cf5ebe9480c078b23be9558cfdc51
Muhammadd08 <sobhynuhammad8@gmail.com.com>
2023-10-01 06:36:01 -0700

    S

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,17 @@
+/**
+ * get_bit - returns the value of a bit at a given index.
+ * @n: number we want to calculate it's digit.
+ * @index: the index, starting from 0 of the bit you want to get.
+ * Return: the value of the bit at index index,
+ *or -1 if an error occured.
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned int i;
+
+	for (i = 0; i != index; i++)
+		n = n >> 1;
+	if (n & 1)
+		return (1);
+	return (0);
+}
