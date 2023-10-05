13862726b6ed2c0b835ab84aaa7246394c031f10
Muhammadd08 <sobhynuhammad8@gmail.com.com>
2023-10-05 10:08:33 -0700

    D

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -15,19 +15,19 @@ int main(int argc, char **argv)
 
 	if (argc != 3)
 	{
-		dprintf(2, "Usage: cp %s %s\n", argv[1], argv[2]);
+		dprintf(STDERR_FILENO, "Usage: cp %s %s\n", argv[1], argv[2]);
 		exit(97);
 	}
 	fd_r = open(argv[1], O_RDONLY);
 	if (fd_r == -1)
 	{
-		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 	fd_w = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fd_w == -1)
 	{
-		dprintf(2, "Error: Can't write to1 %s\n", argv[2]);
+		dprintf(STDERR_FILENO, "Error: Can't write to1 %s\n", argv[2]);
 		exit(99);
 	}
 	while (count_r == 1024)
@@ -35,13 +35,13 @@ int main(int argc, char **argv)
 		count_r = read(fd_r, buff, 1024);
 		if (count_r == -1)
 		{
-			dprintf(2, "Error: Can't read from file %s\n", argv[1]);
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 			exit(98);
 		}
 		count_w = write(fd_w, buff, count_r);
 		if (count_w == -1)
 		{
-			dprintf(2, "Error: Can't write to2 %s\n", argv[2]);
+			dprintf(STDERR_FILENO, "Error: Can't write to2 %s\n", argv[2]);
 			exit(99);
 		}
 
