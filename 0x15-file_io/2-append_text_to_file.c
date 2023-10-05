585fc1f047b8c24f8f59ce9ee761faea631fa94e
Muhammadd08 <sobhynuhammad8@gmail.com.com>
2023-10-05 02:28:36 -0700

    C

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -13,13 +13,13 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	fd = open(filename, O_WRONLY | O_APPEND);
+	if (text_content == NULL)
+		text_content = "";
+	fd = open(filename, O_APPEND);
 	if (fd == -1)
 		return (-1);
 	while (text_content[i])
 		i++;
-	if (text_content == NULL)
-		text_content = "";
 	write(fd, text_content, i);
 	close(fd);
 	return (1);
