fd79f2175557a54a0b5c1676fe1c59e9b7079420
Muhammadd08 <sobhynuhammad8@gmail.com.com>
2023-10-05 01:53:14 -0700

    B

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,7 +17,7 @@ int create_file(const char *filename, char *text_content)
 		text_content = "";
 	while (text_content[i])
 		i++;
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fd < 0)
 		return (-1);
 	w = write(fd, text_content, i);
