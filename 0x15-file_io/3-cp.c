280d938dc6d882f733b48a0bf34f50f724a35283
Muhammadd08 <sobhynuhammad8@gmail.com.com>
2023-10-05 10:52:35 -0700

    D

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -14,7 +14,7 @@ int main(int argc, char **argv)
 	char buff[1024];
 
 	if (argc != 3)
-		dprintf(STDERR_FILENO, "Usage: cp %s %s\n", argv[1], argv[2]), exit(97);
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n", exit(97);
 	fd_r = open(argv[1], O_RDONLY);
 	if (fd_r == -1)
 	{
